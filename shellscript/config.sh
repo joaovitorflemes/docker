fa99df90b4890f6692d88f5ece7c2f19d677d10e
joao vitor <fjoaovitor020@gmail.com>
2025-04-14 08:18:02 -0400

    feat: shell script basico

---
+++ shellscript/config.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+apt update
+apt -y upgrade 
+apt -y install nano git
+git clone https://github.com/joaovitorflemes/docker
+cd docker
+ls 
+
+
---
+++ shellscript/variaveis.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+clear
+nome='joao'
+idade=20
+
+echo ola $nome de idade $idade
+
